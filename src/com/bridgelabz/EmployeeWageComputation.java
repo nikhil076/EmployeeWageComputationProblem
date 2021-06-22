1f738a969d63d663496021c0664c065aca89a6fc
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-22 12:47:39 +0530

    Updated: calculated wage till total woking hours or day is reached a month

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,42 +1,42 @@
 package com.bridgelabz;
 
-import com.sun.org.apache.bcel.internal.generic.DADD;
-
 public class EmployeeWageComputation {
 	public static final int IS_FULLTIME_PRESENT = 1;
 	public static final int IS_PARTTIME_PRESENT = 2;
-	public static final int WORKING_DAYS_PER_MONTH = 20;
+	public static final int MAX_HOURS_ALLOWED = 100;
+	public static final int DAYS_IN_A_MONTH = 20;
 	public static final int WAGE_PER_DAY = 20;
 	
-	public static void main(String args[]) {
-		System.out.println("Welcome to wage computation problem");
-		
-		int FULL_DAY = 8;
-		int PART_TIME = 4;
-		int DAILY_WAGE = 0;
+	public static void main(String args[]) {		
+		int TOTAL_WORKING_HOURS = 0;
+		int TOTAL_WORKING_DAYS = 0;
 		int MONTHLY_WAGE = 0;
-		double empCheck = Math.floor(Math.random() * 10 ) % 3;
-		System.out.println(empCheck);
-
-		switch ((int)empCheck) {
-		case IS_FULLTIME_PRESENT : 
-			DAILY_WAGE = WAGE_PER_DAY * FULL_DAY;
-			MONTHLY_WAGE = DAILY_WAGE * WORKING_DAYS_PER_MONTH ;
-			System.out.println("Employee is present for full day");
-			System.out.println("Employee Daily wage for full day is :"+DAILY_WAGE);
-			System.out.println("Employee Monthy Wage for Full day is :"+MONTHLY_WAGE);
-			break;
-			
-		case IS_PARTTIME_PRESENT :
-			DAILY_WAGE = WAGE_PER_DAY * PART_TIME;
-			MONTHLY_WAGE = DAILY_WAGE * WORKING_DAYS_PER_MONTH;
-			System.out.println("Employee is present for part time");
-			System.out.println("Employee daily wagefor part time is :"+DAILY_WAGE);
-			System.out.println("Employee monthly wwage for part time is :"+MONTHLY_WAGE);
-			break;
 		
-		default:
-			System.out.println("Employee is absent");
+		while(TOTAL_WORKING_HOURS<MAX_HOURS_ALLOWED && TOTAL_WORKING_DAYS<DAYS_IN_A_MONTH){
+			
+			int DAILY_WAGE = 0;
+			TOTAL_WORKING_DAYS++;
+			int EMP_HOURS=0;
+			
+			double empCheck = Math.floor(Math.random() * 10 ) % 3;			
+			switch ((int)empCheck) {
+			case IS_FULLTIME_PRESENT : 
+				EMP_HOURS = 8;
+				break;
+				
+			case IS_PARTTIME_PRESENT :
+				EMP_HOURS = 4;
+				break;
+			
+			default:
+				EMP_HOURS = 0;
+			}
+			DAILY_WAGE = WAGE_PER_DAY * EMP_HOURS;
+			TOTAL_WORKING_HOURS += EMP_HOURS;
+			MONTHLY_WAGE = MONTHLY_WAGE + DAILY_WAGE;
 		}
+		System.out.println("Total Working Days :"+TOTAL_WORKING_DAYS);
+		System.out.println("Total Wokking hours :"+TOTAL_WORKING_HOURS);
+		System.out.println("Monthly employee wage :"+MONTHLY_WAGE);
 	}
 }
