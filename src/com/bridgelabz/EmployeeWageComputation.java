cb159f300d3d03d323d0eb0bf4c7e7f859597071
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-22 09:37:28 +0530

    add part time employee attendance and daily wage

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -1,20 +1,30 @@
 package com.bridgelabz;
 
+import com.sun.org.apache.bcel.internal.generic.DADD;
+
 public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		System.out.println("Welcome to wage computation problem");
-		int IS_PRESENT = 1;
+		int IS_FULLTIME_PRESENT = 1;
+		int IS_PARTTIME_PRESENT = 2;
 		int WAGE_PER_DAY = 20;
 		int FULL_DAY = 8;
+		int PART_TIME = 4;
 		int DAILY_WAGE = 0;
-		double empCheck = Math.floor(Math.random() * 10 ) % 2;
+		double empCheck = Math.floor(Math.random() * 10 ) % 3;
 		System.out.println(empCheck);
 		
-		if (empCheck == IS_PRESENT)
+		if (empCheck == IS_FULLTIME_PRESENT)
 		{
 			DAILY_WAGE = WAGE_PER_DAY * FULL_DAY;
-			System.out.println("Employee is present");
+			System.out.println("Employee is present for full day");
 			System.out.println("Employee Daily wage for full day is :"+DAILY_WAGE);
+		}
+		else if (empCheck == IS_PARTTIME_PRESENT)
+		{
+			DAILY_WAGE = WAGE_PER_DAY * PART_TIME;
+			System.out.println("Employee is present for part time");
+			System.out.println("Employee daily wagefor part time is :"+DAILY_WAGE);
 		}
 		else
 		{
