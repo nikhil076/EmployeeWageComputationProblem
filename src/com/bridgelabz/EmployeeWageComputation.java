503e234ffec633e95d072c6236a5c6cc677f7f33
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-22 09:23:15 +0530

    added: employee daily wage for full day

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -4,12 +4,17 @@ public class EmployeeWageComputation {
 	public static void main(String args[]) {
 		System.out.println("Welcome to wage computation problem");
 		int IS_PRESENT = 1;
+		int WAGE_PER_DAY = 20;
+		int FULL_DAY = 8;
+		int DAILY_WAGE = 0;
 		double empCheck = Math.floor(Math.random() * 10 ) % 2;
 		System.out.println(empCheck);
 		
 		if (empCheck == IS_PRESENT)
 		{
+			DAILY_WAGE = WAGE_PER_DAY * FULL_DAY;
 			System.out.println("Employee is present");
+			System.out.println("Employee Daily wage for full day is :"+DAILY_WAGE);
 		}
 		else
 		{
